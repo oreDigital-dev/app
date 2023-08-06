05f06848611bb614fe167682f858e8af34dd43e4
valens200 <valensniyonsenga2003@gmail.com>
2023-08-06 16:29:33 +0200

    [fix]: fixed client rendering issue

--- utils/dataAssets.ts
+++ utils/dataAssets.ts
@@ -35,4 +35,4 @@ export const districts : String[] = [
 
 
 // export const baseUrli = "http://localhost:5000/api/v1"
-export const baseUrl = "https://ore-d-iot.onrender.com/api/v1"
+export const baseUrli = "https://ore-d-iot.onrender.com/api/v1"
