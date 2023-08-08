5ec935d9b1d021e9c098ca527c656db239a965e4
Ingabire Divine <divineingabire69@gmail.com>
2023-08-08 11:20:16 +0200

    worked on site details

--- components/units/registeredSite.tsx
+++ components/units/registeredSite.tsx
@@ -10,20 +10,12 @@ export default function RegisteredSite(props:RegisteredSiteProps) {
     <div className="flex items-center p-3 hover:bg-grey-200 hover:shadow-xl min-w-[300px]  gap-4 border cursor-pointer rounded-lg shadow-sm">
       {/* <Image src={props.img} alt={props.name} width={100} height={100} className="w-auto" /> */}
       <div>
-        <div className='h-20 w-20 bg-gray-200 rounded-md'>
-
-        </div>
-        <SectionHead title={props.name} desc={props.location} />
-        <p className='text-gray-400 text-sm'>Lat: 33.99 E Long: 44.56</p> 
-        <button
-         
-         className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app"
-         
-         >
-           <Link href="/d/sites/detailsSite">
-            <span>Details</span>
-            </Link>
-          </button>
+        <div className="h-20 w-20 bg-gray-200 rounded-md"></div>
+        <SectionHead title={props.minesiteName} desc={props.location} />
+        <p className="text-gray-400 text-sm">Lat: 33.99 E Long: 44.56</p>
+        <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
+          <span>Details</span>
+        </button>
       </div>
     </div>
   );
