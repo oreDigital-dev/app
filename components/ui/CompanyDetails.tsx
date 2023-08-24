6b56ea0438f2dd213d8bfd808d9f3e28fc6bb2ef
MuhireIghor <muhireighor123@gmail.com>
2023-08-24 14:33:02 +0200

    [fix]Changes made on the company details page

--- components/ui/CompanyDetails.tsx
+++ components/ui/CompanyDetails.tsx
@@ -12,42 +12,41 @@ const CompanyDetails2 = (props: CompanyDetailsProps) => {
   return (
     <div>
       {isCompanyDetailsVisible && (
-      <div className="mx-auto shadow-lg bg-white rounded-md absolute w-[40vw] bottom-16 left-32 p-12 space-y-4">
-        <h6 className="text-black-500 font-bold w-full text-center">
-          {props.companyName}
-        </h6>
-        <div className="flex items-center justify-center gap-12">
-          <div className="space-y-4">
-            <SitesIcon />
-            <div className="text-xl font-semibold text-black-500">
-              HQ:{props.districtLocation}
+        <div className="mx-auto shadow-lg bg-white rounded-md absolute w-[30vw] bottom-12 left-48 px-12 pb-12 pt-6 space-y-8">
+          <h6 className="text-black-500 font-bold w-full text-center">
+            {props.companyName}
+          </h6>
+          <div className="flex items-center justify-center gap-12">
+            <div className="space-y-2">
+              <SitesIcon />
+              <div className="text-xs font-medium text-gray-500">
+                HQ:{props.districtLocation}
+              </div>
+            </div>
+            <div className="w-1 h-12 bg-gray-200 " />
+            <div className="">
+              <h6 className="text-lg  font-semibold text-black-500">
+                +{props.activeSites}
+              </h6>
+              <p className="text-xs font-medium text-gray-500">Active sites</p>
             </div>
           </div>
-          <div className="w-1 h-12 bg-gray-200 " />
-          <div className="space-y-4">
-            <h6 className="text-xl  font-semibold text-black-500">
-              +{props.activeSites}
-            </h6>
-            <p className="text-xl font-semibold text-black-500">Active sites</p>
+          <div>
+            <p className="text-gray-500 font-medium">{props.description}</p>
+            <p className="text-gray-500 font-medium">{props.description}</p>
+          </div>
+          <div className="flex gap-4 justify-center">
+            <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
+              <span>more details</span>
+            </button>
+            <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
+              <span>View sites</span>
+            </button>
+            <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
+              <span>Contact</span>
+            </button>
           </div>
         </div>
-        <div>
-          <p className="text-black-500 font-medium">{props.description}</p>
-          <p className="text-black-500 font-medium">{props.description}</p>
-        </div>
-        <div className="flex gap-4">
-          <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
-            <span>more details</span>
-          </button>
-          <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
-            <span>View sites</span>
-          </button>
-          <button className="py-3 mt-3 flex items-center  gap-2 px-4 rounded-full bg-app/10  hover:bg-app/30  text-app fill-app">
-            <span>Contact</span>
-          </button>
-        </div>
-      </div>
-
       )}
     </div>
   );
