a46193749cef2f60981c1645ef435c138a57ae3f
Ingabire Divine <divineingabire69@gmail.com>
2023-09-07 11:53:02 +0200

    Resolved errors

--- utils/dataAssets.ts
+++ utils/dataAssets.ts
@@ -1,4 +1,4 @@
-import { CompanyDetails, CompanyRecords, CpyReports, ExpandedCompanyDetails, MoreCompanyDetails, RmbDetails } from "@/@types/interfaces";
+import { CompanyRecords, ExpandedCompanyDetails, MoreCompanyDetails } from "@/@types/interfaces";
 
 import { AccountsIcon, DashBoardIcon, EmployeesIcon, LogsIcon, ReportsIcon, SettingsIcon, SitesIcon, SupportIcon, feedBackIcon, helpIcon, logoutIcon } from "@/components/icons";
 export let countryList: String[] = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
@@ -387,228 +387,56 @@ export const rmbLinks: DashBoardSection[] = [
 ]
 
 
-export const companyDetails: CompanyDetails[] = [
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 1,
-  },
-  {
-    companyName: "LUNA SMELTER",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 2,
-  },
-  {
-    companyName: "AL-KAREEM Ltd",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 3,
-  },
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 4,
-  },
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 5,
-  },
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 6,
-  },
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 7,
-  },
-  {
-    companyName: "RMC",
-    hqName: "Muhabura,Gihanga",
-    licenseType: "Mineral processing",
-    id: 8
-  },
-]
-
-export const reportsData: CpyReports[] = [
-  {
-    id: 1,
-    companyName: "LUNA SMELTER",
-    companyReports: [
-      {
-        id: 1,
-        issuedDate: "12 May 2023",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf"
-      },
-      {
-        id: 2,
-        issuedDate: "15 June 2023",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf"
-      },
-      {
-        id: 3,
-        issuedDate: "15 June 2023",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf"
-      },
-      {
-        id: 4,
-        issuedDate: "15 June 2023",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf"
-      },
-      {
-        id: 5,
-        issuedDate: "15 June 2023",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf"
-      },
-    ]
-  },
-  {
-    id: 2,
-    companyName: "AL-KAREEM Ltd",
-    companyReports: [
-      {
-        id: 6,
-        issuedDate: "23 September 2022",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-      },
-      {
-        id: 7,
-        issuedDate: "23 September 2022",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-      },
-      {
-        id: 8,
-        issuedDate: "23 September 2022",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-      },
-      {
-        id: 9,
-        issuedDate: "23 September 2022",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-      },
-      {
-        id: 10,
-        issuedDate: "23 September 2022",
-        reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-      },
-    ]
-  }
-]
-export const reports = [
-  {
-    id: 1,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 2,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 3,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 4,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 5,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 6,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 7,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 8,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 9,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-  {
-    id: 10,
-    issuedDate: "23 September 2022",
-    reportName: "Temperature_rise_report-2023-08-10_Mashyuza-zinc-mine.pdf",
-  },
-]
-
-export const notificationLength: String = "Hello your minesite has quired the highest temperature  highest temperature  highest temperature"
-
-
-
-export const piData = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-];
-export const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
-export const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-export const notificationLength: String = "Hello your minesite has quired the highest temperature  highest temperature  highest temperature"
+ export  const piData = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+  ];
+ export  const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
+ export  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+export const notificationLength : String = "Hello your minesite has quired the highest temperature  highest temperature  highest temperature"
 // export const baseUrli = "http://localhost:5000/api/v1"
 export const baseUrli = "https://ore-d-iot.onrender.com/api/v1"
