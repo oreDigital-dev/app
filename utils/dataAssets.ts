3078ad3d7aab2f0e4886d024f1571237458499cd
MuhireIghor <muhireighor123@gmail.com>
2023-09-11 14:10:07 +0200

    [feat]:Auth api integration

--- utils/dataAssets.ts
+++ utils/dataAssets.ts
@@ -1,6 +1,6 @@
 import { CompanyDetails, CompanyRecords, CompanyReports, ExpandedCompanyDetails, MoreCompanyDetails, RmbDetails } from "@/@types/interfaces";
 
-import { AccountsIcon, DashBoardIcon, EmployeesIcon, LogsIcon, ReportsIcon, SettingsIcon, SitesIcon, SupportIcon, feedBackIcon, helpIcon, logoutIcon } from "@/components/icons";
+import { AccountsIcon, DashBoardIcon, EmployeesIcon, FormIcon, LogsIcon, ProductionIcon, ReportsIcon, SettingsIcon, SitesIcon, SupportIcon, TagIcon, TraceabilityIcon, feedBackIcon, helpIcon, logoutIcon } from "@/components/icons";
 export let countryList: String[] = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
 export const districts: String[] = [
   'Gasabo',
@@ -365,6 +365,33 @@ export const moreCompanyDetailsData: MoreCompanyDetails[] = [
         dateOfCreation: "12 May 2026"
       }
     ]
+  },
+]
+export const mfoLinks:DashBoardSection[] = [
+  {
+    title:"Dashboard",
+    icon:DashBoardIcon,
+    url:"/"
+  },
+  {
+    title:"Forms",
+    icon:FormIcon,
+    url:"forms"
+  },
+  {
+    title:"Tags",
+    icon:TagIcon,
+    url:"tags"
+  },
+  {
+    title:"Production",
+    icon:ProductionIcon,
+    url:"production"
+  },
+  {
+    title:"Traceability",
+    icon:TraceabilityIcon,
+    url:"traceabililty"
   },
 ]
 export const rmbLinks: DashBoardSection[] = [
