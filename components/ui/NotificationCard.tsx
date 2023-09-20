62d75a0d5e62bf4a7aba05654c7dc1ad5ea53865
MuhireIghor <muhireighor123@gmail.com>
2023-09-20 11:28:46 +0200

    [fix]Changed the notification layout interface structure

--- components/ui/NotificationCard.tsx
+++ components/ui/NotificationCard.tsx
@@ -1,29 +1,27 @@
 import Image from "next/image";
 
 export interface NotificationCardProps {
-  notificationOwner: string;
-  notificationMessage: string;
-  notificationStatus: string;
+
+  message: string;
+  status: number;
 }
 const NotificationCard = (props: NotificationCardProps) => {
   return (
     <div className="bg-white flex items-center justify-evenly w-full p-4 gap-4 rounded-md shadow-md">
-      {props.notificationStatus == "read" &&(
-        <div className="w-4 h-4 bg-white rounded-full border border-app " />
-      ) }
-      {props.notificationStatus == 'unread' &&(
+   
+      {props.status == 0 &&(
        
         <div className="w-4 h-4 bg-app rounded-full border border-app animate-ping" />
       )}
       {
-        props.notificationStatus == 'archieved' &&(
+        props.status ==1 &&(
        
             <div className="w-4 h-4 bg-green-400 rounded-full border border-green-400" />
           )
       }
       <div>
-        <p className="text-gray-500">{props.notificationMessage}</p>
-        <p className="text-app font-medium">{props.notificationOwner}</p>
+        <p className="text-gray-500 w-3/4 ">{props.message}</p>
+
       </div>
       <div className="w-12 h-12 rounded-full flex justify-center items-center bg-gray-400/20 relative">
 <Image src={"/backGround.jpg"} alt="image" fill className="rounded-full" />
